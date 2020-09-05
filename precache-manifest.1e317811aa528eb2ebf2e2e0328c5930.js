8d4f14724bbc4a466c58a08f39f5cc4c85ae73eb
Akshay Kumar <akshaysrinivasan97@gmail.com>
2020-09-04 19:25:11 -0500

    Updates

---
+++ precache-manifest.1e317811aa528eb2ebf2e2e0328c5930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b7825431a03983f6041",
+    "url": "/portfolio/static/css/main.eea45441.chunk.css"
+  },
+  {
+    "revision": "1b7825431a03983f6041",
+    "url": "/portfolio/static/js/main.1b782543.chunk.js"
+  },
+  {
+    "revision": "45d61d37ad8a6d02601e",
+    "url": "/portfolio/static/css/1.f4082eec.chunk.css"
+  },
+  {
+    "revision": "45d61d37ad8a6d02601e",
+    "url": "/portfolio/static/js/1.45d61d37.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "9498bbd306b018ba8b164b4bfd9c5708",
+    "url": "/portfolio/index.html"
+  }
+];
